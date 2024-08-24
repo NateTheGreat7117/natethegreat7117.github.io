229b60a49441ee2982519216a894e86129e57d76
Nathan Montanez <71854770+NateTheGreat7117@users.noreply.github.com>
2024-08-24 16:56:28 -0400

    Delete website directory

--- website/kill_kernel.py
+++
@@ -1,9 +0,0 @@
-import os
-import signal
-
-# Function to restart the kernel
-def restart_kernel():
-    os.kill(os.getpid(), signal.SIGINT)
-
-# Call the function to restart the kernel
-restart_kernel()
